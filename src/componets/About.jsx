b9eac87de88010fde3ec684e4bc9ed0a98ddc20f
Yerovic Pino <jerlpino@misena.edu.co>
2023-12-27 18:06:41 -0500

    ultimo commit.1  nuevo_ portafolio

--- src/componets/About.jsx
+++ src/componets/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
           <div className="flex space-x-0">
             <a
               a
-              href="https://drive.google.com/file/d/1kPPNiRk2WdB0nvCc1sxer4bnYacVNbN-/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1WLY7B9_pu0qyQS7SKsj1LPWw8RBGd8EG/view?usp=drive_link"
               target="_blank"
               rel="noopener noreferrer"
             >
